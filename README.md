e5731f7ba2ef4464a478fbbe8ffbf49f731ee60d
AstroWizardy <AstroWizardry7334@gmail.com>
2025-12-28 00:57:23 +0330

    day 2 part 2 completed

--- README.md
+++ README.md
@@ -12,4 +12,10 @@ To run the desired file example day-01-1.ts:
 bun run day-01-1.ts
 ```
 
+Also if you want to play around I recommend using:
+
+```bash
+bun --watch run day-01-1.ts
+```
+
 This project was created using `bun init` in bun v1.2.20. [Bun](https://bun.com) is a fast all-in-one JavaScript runtime.
--- day-02-2.ts
+++ day-02-2.ts
@@ -1,1 +1,49 @@
-//start
+import { getAocInput } from "./utils/aoc-input";
+
+async function dbSanitizerImproved(): Promise<number> {
+    const text: string = await getAocInput(2);
+    const ranges: string[] = text.trim().split(",");
+
+    function isInvalidIdImproved(id: number): boolean {
+        const str = id.toString();
+        const len = str.length;
+
+        for (let repeated = 2; repeated <= len; repeated++) {
+            // this is good check to skips unecessary checking of some unresonable length 
+            if (len % repeated !== 0) continue;
+
+            const segmentLength = len / repeated;
+            const firstSegment = str.slice(0, segmentLength);
+
+            let allMatch = true;
+            for (let i = 1; i < repeated; i++) {
+                const segment = str.slice(i * segmentLength, (i + 1) * segmentLength);
+                if (segment !== firstSegment) {
+                    allMatch = false;
+                    break;
+                }
+            }
+            if (allMatch) return true;
+        }
+        return false;
+    }
+
+    const invalidIds: number[] = [];
+
+    for (const range of ranges) {
+        const [start = 0, end = 0] = range.split("-").map(Number);
+
+        for (let id: number = start; id <= end; id++) {
+            if(isInvalidIdImproved(id)) {
+                invalidIds.push(id);
+            }
+        }
+    }
+
+    const sum = invalidIds.reduce((total, id) => total + id, 0);
+    return sum;
+}
+
+dbSanitizerImproved().then((sum: number) => {
+    console.log("The new and improved sum of all invalid IDs in db is:", sum);
+})
